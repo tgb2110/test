5cb351fafecb5cc2fa90c29e8c88e8b8d3a0758c
Troy Barrett <OB.Troy@me.com>
2014-05-27 14:26:20 -0400

    Initial Commit

---
+++ GitUseExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitUseExample
+//
+//  Created by Troy Barrett on 5/27/14.
+//  Copyright (c) 2014 OB_Troy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitUseExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitUseExample
+//
+//  Created by Troy Barrett on 5/27/14.
+//  Copyright (c) 2014 OB_Troy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
